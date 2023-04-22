b978f9220dd653cbd8af44f587c469cdbff34915
math-araujo <araujo_m_@hotmail.com>
2023-04-21 23:50:16 -0300

    Add Vertex Array class

---
+++ src/vertex_array.cpp
@@ -0,0 +1,73 @@
+#include "vertex_array.hpp"
+#include <algorithm>
+#include <cassert>
+
+namespace glforge
+{
+
+VertexArray::VertexArray(std::span<const GLint> vertex_layout, GLuint buffer_binding_point)
+{
+    glCreateVertexArrays(1, &_id);
+    assert(_id != 0);
+
+    GLuint offset{0};
+    for (GLuint index = 0; GLint attribute_size : vertex_layout)
+    {
+        glEnableVertexArrayAttrib(_id, index);
+        glVertexArrayAttribFormat(_id, index, attribute_size, GL_FLOAT, false, offset * sizeof(float));
+        glVertexArrayAttribBinding(_id, index, buffer_binding_point);
+        offset += attribute_size;
+        ++index;
+    }
+}
+
+VertexArray::VertexArray(std::span<const GLint> vertex_layout, std::span<const GLuint> buffer_binding_points)
+{
+    glCreateVertexArrays(1, &_id);
+    assert(_id != 0);
+
+    const GLuint num_attributes{static_cast<GLuint>(vertex_layout.size())};
+    for (GLuint index = 0; index < num_attributes; ++index)
+    {
+        glEnableVertexArrayAttrib(_id, index);
+        glVertexArrayAttribFormat(_id, index, vertex_layout[index], GL_FLOAT, false, 0);
+        glVertexArrayAttribBinding(_id, index, buffer_binding_points[index]);
+    }
+}
+
+VertexArray::VertexArray(VertexArray&& rhs) noexcept : _id{rhs._id}
+{
+    rhs._id = 0;
+}
+
+VertexArray& VertexArray::operator=(VertexArray&& rhs) noexcept
+{
+    std::swap(_id, rhs._id);
+    return *this;
+}
+
+VertexArray::~VertexArray()
+{
+    if (_id)
+    {
+        glDeleteVertexArrays(1, &_id);
+    }
+}
+
+GLuint VertexArray::id() const noexcept
+{
+    return _id;
+}
+
+void VertexArray::bind()
+{
+    assert(_id != 0);
+    glBindVertexArray(_id);
+}
+
+void VertexArray::bind_vertex_buffer(GLuint vbo, GLuint vertex_buffer_stride, GLuint buffer_binding_point)
+{
+    glVertexArrayVertexBuffer(_id, buffer_binding_point, vbo, 0, vertex_buffer_stride * sizeof(float));
+}
+
+} // namespace glforge
---
+++ src/vertex_array.hpp
@@ -0,0 +1,37 @@
+#ifndef GL_FORGE_VERTEX_ARRAY_HPP
+#define GL_FORGE_VERTEX_ARRAY_HPP
+
+#include <glad/glad.h>
+#include <span>
+
+namespace glforge
+{
+
+class VertexArray
+{
+public:
+    // Binds a vertex array at buffer_binding_point. Use this constructor for interleaved attributes.
+    // vertex_layout specifies the sizes of the attributes.
+    explicit VertexArray(std::span<const GLint> vertex_layout, GLuint buffer_binding_point = 0);
+
+    // Binds a vertex array with multiple binding points, where the i-th attribute uses the i-th binding point.
+    // vertex_layout specifies the sizes of the attributes.
+    VertexArray(std::span<const GLint> vertex_layout, std::span<const GLuint> buffer_binding_points);
+
+    VertexArray(const VertexArray&) = delete;
+    VertexArray(VertexArray&& rhs) noexcept;
+    VertexArray& operator=(const VertexArray&) = delete;
+    VertexArray& operator=(VertexArray&& rhs) noexcept;
+    ~VertexArray();
+
+    [[nodiscard]] GLuint id() const noexcept;
+    void bind();
+    void bind_vertex_buffer(GLuint vbo, GLuint vertex_buffer_stride, GLuint buffer_binding_point = 0);
+
+private:
+    GLuint _id{0};
+};
+
+} // namespace glforge
+
+#endif // GL_FORGE_VERTEX_ARRAY_HPP
