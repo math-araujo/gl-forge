a7ce6778a42d6eb3ac787b24c65a962dcaf4a8c7
math-araujo <araujo_m_@hotmail.com>
2023-04-22 00:25:47 -0300

    Add Vertex Buffer class

---
+++ src/vertex_buffer.cpp
@@ -0,0 +1,44 @@
+#include "vertex_buffer.hpp"
+#include <algorithm>
+#include <cassert>
+
+namespace glforge
+{
+
+VertexBuffer::VertexBuffer(std::span<const float> buffer_data, GLuint stride) : _stride{stride}
+{
+    assert(_stride > 0);
+    glCreateBuffers(1, &_id);
+    glNamedBufferStorage(_id, buffer_data.size_bytes(), buffer_data.data(), GL_DYNAMIC_STORAGE_BIT);
+}
+
+VertexBuffer::VertexBuffer(VertexBuffer&& rhs) noexcept : _id{rhs._id}
+{
+    rhs._id = 0;
+}
+
+VertexBuffer& VertexBuffer::operator=(VertexBuffer&& rhs) noexcept
+{
+    std::swap(_id, rhs._id);
+    return *this;
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    if (_id)
+    {
+        glDeleteBuffers(1, &_id);
+    }
+}
+
+GLuint VertexBuffer::id() const noexcept
+{
+    return _id;
+}
+
+GLuint VertexBuffer::stride() const noexcept
+{
+    return _stride;
+}
+
+} // namespace glforge
---
+++ src/vertex_buffer.hpp
@@ -0,0 +1,30 @@
+#ifndef GL_FORGE_VERTEX_BUFFER_HPP
+#define GL_FORGE_VERTEX_BUFFER_HPP
+
+#include <glad/glad.h>
+#include <span>
+
+namespace glforge
+{
+
+class VertexBuffer
+{
+public:
+    VertexBuffer(std::span<const float> buffer_data, GLuint stride);
+    VertexBuffer(const VertexBuffer&) = delete;
+    VertexBuffer(VertexBuffer&& rhs) noexcept;
+    VertexBuffer& operator=(const VertexBuffer&) = delete;
+    VertexBuffer& operator=(VertexBuffer&& rhs) noexcept;
+    ~VertexBuffer();
+
+    [[nodiscard]] GLuint id() const noexcept;
+    [[nodiscard]] GLuint stride() const noexcept;
+
+private:
+    GLuint _id{0};
+    GLuint _stride{0};
+};
+
+} // namespace glforge
+
+#endif // GL_FORGE_VERTEX_BUFFER_HPP
