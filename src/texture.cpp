1c3b00b7b283e793cc4bc71f30228973ab1cd067
math-araujo <araujo_m_@hotmail.com>
2023-04-26 23:12:36 -0300

    Add texture storage wrapper class

---
+++ src/texture.cpp
@@ -0,0 +1,50 @@
+#include "texture.hpp"
+#include <algorithm>
+#include <cassert>
+
+namespace glforge
+{
+
+Texture::Texture(std::uint32_t width, std::uint32_t height, const TextureParameters& parameters) :
+    _width{width}, _height{height}
+{
+    assert(_width > 0);
+    assert(_height > 0);
+    glCreateTextures(parameters.target, 1, &_id);
+    assert(_id != 0);
+    glTextureStorage2D(_id, parameters.mip_levels, parameters.internal_format, _width, _height);
+}
+
+Texture::Texture(Texture&& rhs) noexcept : _id{rhs._id}, _width{rhs._width}, _height{rhs._height}
+{
+    rhs._id = 0;
+}
+
+Texture& Texture::operator=(Texture&& rhs) noexcept
+{
+    std::swap(_id, rhs._id);
+    std::swap(_width, rhs._width);
+    std::swap(_height, rhs._height);
+    return *this;
+}
+
+Texture::~Texture()
+{
+    if (_id)
+    {
+        glDeleteTextures(1, &_id);
+    }
+}
+
+GLuint Texture::id() const noexcept
+{
+    return _id;
+}
+
+void Texture::bind(GLuint sampler, GLuint unit)
+{
+    glBindSampler(unit, sampler);
+    glBindTextureUnit(unit, _id);
+}
+
+} // namespace glforge
---
+++ src/texture.hpp
@@ -0,0 +1,38 @@
+#ifndef GL_FORGE_TEXTURE_HPP
+#define GL_FORGE_TEXTURE_HPP
+
+#include <cstdint>
+#include <glad/glad.h>
+
+namespace glforge
+{
+
+struct TextureParameters
+{
+    GLenum target{GL_TEXTURE_2D};
+    GLsizei mip_levels{1};
+    GLenum internal_format{GL_RGBA32F};
+};
+
+class Texture
+{
+public:
+    Texture(std::uint32_t width, std::uint32_t height, const TextureParameters& parameters = {});
+    Texture(const Texture&) = delete;
+    Texture(Texture&& rhs) noexcept;
+    Texture& operator=(const Texture&) = delete;
+    Texture& operator=(Texture&& rhs) noexcept;
+    ~Texture();
+
+    [[nodiscard]] GLuint id() const noexcept;
+    void bind(GLuint sampler, GLuint unit = 0);
+
+private:
+    GLuint _id{0};
+    std::uint32_t _width{0};
+    std::uint32_t _height{0};
+};
+
+} // namespace glforge
+
+#endif // GL_FORGE_TEXTURE_HPP
