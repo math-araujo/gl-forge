14434df9d2ca8270cc0d785043cc7e01526788c0
math-araujo <araujo_m_@hotmail.com>
2023-05-09 23:02:38 -0300

    Update FetchContent population to download vcpkg outside build directory
    Source:
    https://discourse.cmake.org/t/built-in-package-manager-for-cmake-modules/7513/4

--- cmake/fetchvcpkg.cmake
+++ cmake/fetchvcpkg.cmake
@@ -1,8 +1,11 @@
 include(FetchContent)
-FetchContent_Declare(vcpkg
-    GIT_REPOSITORY https://github.com/microsoft/vcpkg/
-    GIT_TAG 2023.02.24
+FetchContent_Populate(
+  vcpkg
+  GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
+  GIT_TAG        2023.02.24
+  SOURCE_DIR     "${CMAKE_SOURCE_DIR}/vcpkg"
+  QUIET
 )
-FetchContent_MakeAvailable(vcpkg)
 
-set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake" CACHE FILEPATH "")
+list(APPEND CMAKE_PROJECT_TOP_LEVEL_INCLUDES "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_PROJECT_TOP_LEVEL_INCLUDES)
